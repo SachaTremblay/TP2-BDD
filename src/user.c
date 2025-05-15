3582f132f4155909e39ec0fb72de7ac3f17260a1
SachaTremblay <2430055@bart.ca>
2025-05-14 21:02:10 -0400

    From Home To School

---
+++ src/user.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+#include "user.h"
+
+int SaveUser(const char* username, const char* mdp){
+    sqlite3 *db;
+    //sqlite3_stat *stat;
+    int rc;
+
+
+}
